92ca974a0d43213b7bfc88083be7c36b2f50e32f
tal baram <talbaram3192@gmail.com>
2021-01-20 17:40:48 +0200

    implemented json encoding functions in main

--- encode_json.py
+++ encode_json.py
@@ -1,44 +1,74 @@
 import json
-import random
 import requests
+import numpy as np
+import pandas as pd
+import pickle
+import shap
+import matplotlib.pyplot as plt
 
-def convert_json(my_json):
-    new_json = {}
-    json_get = json.loads(my_json)
-    for num, i in enumerate(json_get):
-        if num <= 8:
-            if json_get[i] == 2 or json_get[i] == 3 or json_get[i] == 4:
-                new_json[i] = 1
-            else:
-                new_json[i] = 0
-        elif num == 9:
-            if json_get[i] == 0 or json_get[i] == 1 or json_get[i] == 2:
-                new_json[i] = 1
-            else:
-                new_json[i] = 0
-        elif num == 11:
-            if json_get[i] == 2:
-                new_json[i] = random.randint(0, 1)
-            else:
-                new_json[i] = json_get[i]
-        else:
-            new_json[i] = json_get[i]
 
-    return json.dumps(new_json)
+with open('rand_forest.pickle', 'rb') as m:
+    MODEL = pickle.load(m)
+
+COLUMNS = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'Age_Mons',
+       'Sex', 'Jaundice', 'Family_mem_with_ASD']
+
+
+def get_explanation(pred):  # Dictionary object
+    pred2 = np.array(pd.Series(pred)).reshape(1, len(pred))
+
+    explainer = shap.TreeExplainer(MODEL)
+    shap_values = explainer.shap_values(pred2)
+    shap.initjs()
+    shap.summary_plot(shap_values, pred2, feature_names=COLUMNS, show=False)
+    plt.savefig('summary.png')
+
+    # shap.force_plot(explainer.expected_value[0],
+    #                 shap_values[0][0, :], pred2, feature_names=COLUMNS, show=False)
+    # plt.savefig('force_plot.png')
+
+
+#
+# def convert_json(my_dict):
+#     new_json = {}
+#     print(type(my_dict))
+#     # my_dict = eval(my_dict)
+#     for num, i in enumerate(my_dict):
+#         if num <= 8:
+#             if my_dict[i] == 2 or my_dict[i] == 3 or my_dict[i] == 4:
+#                 new_json[i] = 1
+#             else:
+#                 new_json[i] = 0
+#         elif num == 9:
+#             if my_dict[i] == 0 or my_dict[i] == 1 or my_dict[i] == 2:
+#                 new_json[i] = 1
+#             else:
+#                 new_json[i] = 0
+#         elif num == 11:
+#             if my_dict[i] == 2:
+#                 new_json[i] = random.randint(0, 1)
+#             else:
+#                 new_json[i] = my_dict[i]
+#         else:
+#             new_json[i] = my_dict[i]
+#
+#     return json.dumps(new_json)
 
 
 def main():
 
        my_json = {"A1": 0,"A2": 1,"A3": 4,"A4": 3,"A5": 1, "A6":4, "A7": 2,
                "A8": 0,"A9": 1,"A10": 0,"age_month": 20,"sex": 2,"jaundice": 0,"family_mem_with_ASD": 1}
-       new = convert_json(json.dumps(my_json))
 
-       print(new)
-       url = 'https://spectrum-screen-inference.herokuapp.com/api/predict'
+       # url = 'https://spectrum-screen-inference.herokuapp.com/api/predict'
+       url = 'http://127.0.0.1:5000/api/predict'  # for local testing
 
-       pred = requests.post(url, json=json.dumps(new))
-
+       pred = requests.post(url, json=json.dumps(my_json))
        print(pred.json())
+
+       # get_explanation(my_json)
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify, make_response
+import json
+import random
 
 app = Flask(__name__)
 
 
+def convert_json(my_dict):
+    new_json = {}
+    for num, i in enumerate(my_dict):
+        if num <= 8:
+            if my_dict[i] == 2 or my_dict[i] == 3 or my_dict[i] == 4:
+                new_json[i] = 1
+            else:
+                new_json[i] = 0
+        elif num == 9:
+            if my_dict[i] == 0 or my_dict[i] == 1 or my_dict[i] == 2:
+                new_json[i] = 1
+            else:
+                new_json[i] = 0
+        elif num == 11:
+            if my_dict[i] == 2:
+                new_json[i] = random.randint(0, 1)
+            else:
+                new_json[i] = my_dict[i]
+        else:
+            new_json[i] = my_dict[i]
+
+    return json.dumps(new_json)
+
+
 @app.route("/api/predict", methods=["POST"])
 def predict():
-    return make_response(jsonify({"prediction": 0, "prediction_probability": 0.98}), 200)
+    my_json = request.get_json()
+    my_json = json.loads(my_json)
+    print('Before encoding', my_json)
+    encoded_dict = convert_json(my_json)
+    print('After encoding- ', encoded_dict)
+
+    # return make_response(jsonify({"prediction": 0, "prediction_probability": 0.98}), 200)
+    return make_response(jsonify(encoded_dict), 200)
 
 
 if __name__ == '__main__':
